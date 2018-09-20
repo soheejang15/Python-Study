bc00a8ae91a0d4f3e9fa71552caa1c4e5c4f20b8
soheejang15 <jsh167528@gmail.com>
2018-09-20 23:25:08 +0900

    Move file location

--- function/set_value.py
+++
@@ -1,9 +0,0 @@
-def say_myself(name, old, man = True):
-    print("My name is %s"%name)
-    print("I'm %d years old"%old)
-    if man :
-        print("A man")
-    else :
-        print("A woman")
-
-say_myself("ABC",17)
