7c287483d83b5767ff31ad94264fa6f43a3fc7d0
soheejang15 <jsh167528@gmail.com>
2018-09-21 23:00:39 +0900

    Class Constructor

---
+++ 05-1_Class/05-1-5.py
@@ -0,0 +1,16 @@
+# 5) Calculator 3
+
+class Calculator :
+    def __init__(self, *args):
+        self.result = 0
+        self.arr = args
+
+    def sum (self):
+        for i in arr :
+            self.result += i
+        return self.result
+
+    def avg (self):
+        self.result /= len(arr)
+        return self.result
+        
